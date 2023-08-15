59a08273f9cc7c8a7c4d6630d70fb5449cc7717a
HikmatD <hikmatdhamee@gmail.com>
2023-08-15 15:41:46 +0545

    add send money feature

--- springboot-core-banking/src/main/java/com/hackerrank/corebanking/controller/TransactionController.java
+++ springboot-core-banking/src/main/java/com/hackerrank/corebanking/controller/TransactionController.java
@@ -1,19 +1,25 @@
 package com.hackerrank.corebanking.controller;
 
 import com.hackerrank.corebanking.model.Transaction;
+import com.hackerrank.corebanking.repository.TransactionRepository;
 import com.hackerrank.corebanking.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/core-banking/transaction")
 public class TransactionController {
   private final TransactionService transactionService;
+  private final TransactionRepository transactionRepository;
 
   @Autowired
-  public TransactionController(TransactionService transactionService) {
+  public TransactionController(TransactionService transactionService, TransactionRepository transactionRepository) {
     this.transactionService = transactionService;
+    this.transactionRepository = transactionRepository;
   }
 
   //create
@@ -21,5 +27,12 @@ public class TransactionController {
   @ResponseStatus(HttpStatus.CREATED)
   public Transaction sendMoney(@RequestBody Transaction transaction) {
     return transactionService.sendMoney(transaction);
+  }
+
+  //get
+  @GetMapping("/history")
+  @ResponseStatus(HttpStatus.OK)
+  public List<Transaction> getAccountByAccountId(@RequestParam(name = "fromDate") Date fromDate, @RequestParam(name = "toDate") Date toDate) {
+    return transactionRepository.findByDateCreatedBetween(fromDate, toDate);
   }
 }
--- springboot-core-banking/src/main/java/com/hackerrank/corebanking/repository/TransactionRepository.java
+++ springboot-core-banking/src/main/java/com/hackerrank/corebanking/repository/TransactionRepository.java
@@ -3,5 +3,9 @@ package com.hackerrank.corebanking.repository;
 import com.hackerrank.corebanking.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByDateCreatedBetween(Date from, Date to);
 }
