ea3b78780c28ab3d7d7c70e9d9b883d38882f773
HikmatD <hikmatdhamee@gmail.com>
2023-09-14 21:10:27 +0545

    fix dates

--- backend/src/main/java/com/hackerrank/corebanking/repository/TransactionRepository.java
+++ backend/src/main/java/com/hackerrank/corebanking/repository/TransactionRepository.java
@@ -8,7 +8,7 @@ import java.util.Date;
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    @Query("SELECT t FROM Transaction t WHERE t.dateCreated >=  ?1 AND t.dateCreated <= ?2 AND (t.fromAccountId = ?3 OR t.toAccountId = ?4)")
+    @Query("SELECT t FROM Transaction t WHERE t.dateCreated >=  ?1 AND t.dateCreated < DATEADD(day, 1, CAST(?2 AS DATE)) AND (t.fromAccountId = ?3 OR t.toAccountId = ?4)")
     List<Transaction> findTransactionsByDateCreatedBetweenAndFromAccountIdOrToAccountId(Date from, Date to, String fromAccountId, String toAccountId);
     List<Transaction> findTransactionByFromAccountId(Long fromAccountId);
 }
