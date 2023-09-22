14f77a2528c89631bacfca267fe0e6711e16ead8
HikmatD <hikmatdhamee@gmail.com>
2023-09-22 13:30:56 +0545

    medium feature 1

--- backend/src/main/java/com/hackerrank/corebanking/service/TransactionService.java
+++ backend/src/main/java/com/hackerrank/corebanking/service/TransactionService.java
@@ -25,6 +25,12 @@ public class TransactionService {
     Account fromAccount = accountRepository.findById(transaction.getFromAccountId()).get();
     Account toAccount = accountRepository.findById(transaction.getToAccountId()).get();
 
+    //validate that the sender(fromAccount) and beneficiary(toAccount) is not exceeding maximum amount limit
+    /**
+     * 1. sender cannot send more than 5000 at once to newly added beneficiary.
+     * 2. to send more than 5000 to a beneficiary, beneficiary add date should be at least 5 days back.
+     * 3. if the receiver is not in beneficiary list of sender, then no restriction on send amount.
+     */
     fromAccount.setBalance(fromAccount.getBalance() - transaction.getTransferAmount());
     toAccount.setBalance(toAccount.getBalance() + transaction.getTransferAmount());
 
