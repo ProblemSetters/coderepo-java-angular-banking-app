dc0b7d4cdad1678abb936b652fc399a7ab195bf9
HikmatD <hikmatdhamee@gmail.com>
2023-09-26 20:57:24 +0545

    update cores in master

--- backend/src/main/java/com/hackerrank/corebanking/CorsConfig.java
+++ backend/src/main/java/com/hackerrank/corebanking/CorsConfig.java
@@ -13,9 +13,8 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**") // Specify the API endpoints you want to allow CORS for
                 .allowedOriginPatterns("*") // List your allowed origins here
                 .allowCredentials(true)
-                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
-                        "Access-Control-Request-Headers", "Authorization")
-                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
     }
 }
