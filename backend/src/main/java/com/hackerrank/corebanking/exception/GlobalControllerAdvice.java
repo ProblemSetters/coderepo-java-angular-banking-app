a1c8f3dfc613daddef0c00d943213fde9d64f0f8
HikmatD <hikmatdhamee@gmail.com>
2023-09-30 08:43:20 +0545

    adding basic feature question

---
+++ backend/src/main/java/com/hackerrank/corebanking/exception/GlobalControllerAdvice.java
@@ -0,0 +1,12 @@
+package com.hackerrank.corebanking.exception;
+
+public class GlobalControllerAdvice {
+    //TODO: Implement it
+    /**
+     * 1. annotate this class with necessary annotations so that it catches all exceptions
+     * 2. write a method that can handle all the app's exceptions
+     * 3. response status should be 500.
+     * 3. return the exception message as JSON like
+     *  {"message": "exception message"}
+     */
+}
