272ec97529fad4e2d36fc22d6cddb664aee91972
Parth Faladu <parthfaladu@Parths-Mac-mini.local>
2023-10-13 18:04:31 +0530

    get/set user detail on page refresh functionality set

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ToastrService } from "ngx-toastr";
+import { HttpErrorResponse } from '@angular/common/http';
+import { AccountService } from 'src/app/services/account.service';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'banking-system-app';
+  
+  constructor(
+		private toastr: ToastrService,
+		private accountService: AccountService,
+    private authenticationService: AuthenticationService,
+	) {}
+
+  ngOnInit(): void {
+    this.getUserAccount();
+  }
+
+  getUserAccount() {
+		this.accountService.getUserAsAccount().subscribe(
+      {
+        next: (data: any) => {
+          this.authenticationService.setAccount(data);
+        },
+        error: (e: HttpErrorResponse) => {
+          this.toastr.error('Oops! Something went wrong while geting user account info.');
+        },
+        complete: () => {}
+      }
+		)
+  }
 }
