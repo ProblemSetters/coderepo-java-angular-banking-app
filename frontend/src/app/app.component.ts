0be5246cf83ba3ba3fff6f3ae7c4a00c0883c703
Parth Faladu <parthfaladu@Parths-Mac-mini.local>
2023-10-16 10:38:55 +0530

    if user login then get user account detail functionality set

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { ToastrService } from "ngx-toastr";
 import { HttpErrorResponse } from '@angular/common/http';
 import { AccountService } from 'src/app/services/account.service';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,15 +12,25 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 })
 export class AppComponent {
   title = 'banking-system-app';
+  public isAuth: boolean = false;
   
   constructor(
 		private toastr: ToastrService,
 		private accountService: AccountService,
     private authenticationService: AuthenticationService,
+    private router: Router,
 	) {}
 
   ngOnInit(): void {
-    this.getUserAccount();
+    this.authenticationService.isAuthenticate().subscribe((status) => {
+			this.isAuth = status;
+			if (this.isAuth) {
+        this.getUserAccount();
+			}else {
+        this.router.navigate(["login"]);
+      }
+		});
+    
   }
 
   getUserAccount() {
