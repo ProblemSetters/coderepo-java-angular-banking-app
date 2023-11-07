93d5759f02b24f3f5904f11ec89d01df61adee1e
HikmatD <hikmatdhamee@gmail.com>
2023-11-07 19:39:22 +0545

    Merge remote-tracking branch 'origin/master'

--- frontend/src/app/account/account.component.ts
+++ frontend/src/app/account/account.component.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-import {
-	FormControl,
-	FormGroup,
-	Validators
-} from "@angular/forms";
 import { ToastrService } from "ngx-toastr";
 import { AccountService } from 'src/app/services/account.service';
 import { AuthenticationService } from 'src/app/services/authentication.service';
@@ -11,15 +6,35 @@ import { Router } from "@angular/router";
 import { HttpErrorResponse } from '@angular/common/http';
 import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
+interface OpenAccount {
+	firstName: string,
+	lastName: string,
+	dob: object,
+	gender: string,
+	address: string,
+	city: string,
+	emailAddress: string,
+	password: string
+
+}
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.scss']
 })
 export class AccountComponent {
-  public openAccountForm!: FormGroup;
   public isAuth: boolean = false;
   public todayDate: NgbDateStruct = this.getCurrentDate()
+  public openAccount: OpenAccount = {
+	  firstName: '',
+	  lastName: '',
+	  dob: this.todayDate,
+	  gender: 'male',
+	  address: '',
+	  city: '',
+	  emailAddress: '',
+	  password: ''
+  }
   
   getCurrentDate(): NgbDateStruct {
     const today = new Date();
@@ -29,6 +44,7 @@ export class AccountComponent {
       day: today.getDate(),
     };
   }
+
    constructor(
 		private toastr: ToastrService,
 		private accountService: AccountService,
@@ -45,85 +61,21 @@ export class AccountComponent {
 	if (this.isAuth) {
 		this.router.navigate([""]);
 	}
-	this.openAccountForm = new FormGroup({
-      firstName: new FormControl(null, Validators.required),
-      lastName: new FormControl(null, Validators.required),
-      dob: new FormControl(null, Validators.required),
-      gender: new FormControl('male', Validators.required),
-      address: new FormControl(null, Validators.required),
-      city: new FormControl(null, Validators.required),
-      emailAddress: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, [
-				Validators.required,
-				Validators.minLength(6),
-			]),
-      privacyPolicy: new FormControl(false, Validators.requiredTrue),
-		});
-	}
+  }
 
-  getFormControlError(fieldName: string): string {
-		const field = this.openAccountForm.get(fieldName);
-		if (field && field.touched && field.invalid) {
-			if (field.errors?.["required"]) {
-				if(fieldName === 'firstName'){
-					return 'Please enter a first name. It is required.';
-				}
-				if(fieldName === 'lastName'){
-					return 'Please enter a last name. It is required.';
-				}
-				if(fieldName === 'dob'){
-					return 'Please enter a date of birth. It is required.';
-				}
-				if(fieldName === 'gender'){
-					return 'Please enter a gender. It is required.';
-				}
-				if(fieldName === 'address'){
-					return 'Please enter a address. It is required.';
-				}
-				if(fieldName === 'city'){
-					return 'Please enter a city. It is required.';
-				}
-				if(fieldName === 'emailAddress'){
-					return 'Please enter a email address. It is required.';
-				}
-				if(fieldName === 'password'){
-					return 'Please enter a password. It is required.';
-				}
-				if(fieldName === 'privacyPolicy'){
-					return 'Please enter a privacy policy. It is required.';
-				}
-			}
-			if (field.errors?.["minlength"]) {
-				if(fieldName === 'password') {
-					return `Password must be at least ${field.errors["minlength"].requiredLength} characters long.`;
-				}
-			}
-
-			if (field.errors?.["pattern"]) {
-				if (fieldName === "accountId") {
-					return "Invalid account number format. Please enter a valid 12-digit numeric account number.";
-				}
-			}
-		}
-		return "";
-	}
 
   onSubmit() {
-		if (this.openAccountForm.invalid) {
-			this.toastr.error("Please fill in all the required fields.");
-			return;
-		}
-		const dateOfBirth = new Date(this.openAccountForm.get("dob")!.value.year, this.openAccountForm.get("dob")!.value.month - 1, this.openAccountForm.get("dob")!.value.day);
+		const dateOfBirth = new Date(this.todayDate.year, this.todayDate.month - 1, this.todayDate.day);
 		const res = this.accountService
 			.openAccount(
-				this.openAccountForm.get("firstName")!.value,
-				this.openAccountForm.get("lastName")!.value,
+				this.openAccount.firstName,
+				this.openAccount.lastName,
 				dateOfBirth,
-				this.openAccountForm.get("gender")!.value,
-				this.openAccountForm.get("address")!.value,
-				this.openAccountForm.get("city")!.value,
-				this.openAccountForm.get("emailAddress")!.value,
-				this.openAccountForm.get("password")!.value,
+				this.openAccount.gender,
+				this.openAccount.address,
+				this.openAccount.city,
+				this.openAccount.emailAddress,
+				this.openAccount.password,
 			)
 			.subscribe(
 				{
@@ -134,7 +86,7 @@ export class AccountComponent {
 					error: (e: HttpErrorResponse) => {
 						this.toastr.error('Oops! Something went wrong while creating account.');
 					},
-					complete: () => { this.openAccountForm.reset(); }
+					complete: () => {  }
 				}
 			);
 	}
