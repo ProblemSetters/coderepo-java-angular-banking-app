501b8631afa33cad0c9ce124cc3da194f45bf607
Anshu-Pandey-Hackerrank <anshu.pandey@problemsetters.com>
2024-05-02 14:09:51 +0530

    add: basic bugfix validation question

--- backend/src/main/java/com/hackerrank/corebanking/service/TransactionService.java
+++ backend/src/main/java/com/hackerrank/corebanking/service/TransactionService.java
@@ -25,6 +25,13 @@ public class TransactionService {
     Account fromAccount = accountRepository.findById(transaction.getFromAccountId()).get();
     Account toAccount = accountRepository.findById(transaction.getToAccountId()).get();
 
+    // Basic Bugfix Balance Validation
+    // Problem Statement:
+    // 1. the sending account must have a balance greater than or equal to the sending amount.
+    // 2. if balance is insufficient, transaction must be rejected and appropriate message to be returned.
+    // 2. the return message should be "Insufficient fund, transaction canceled".
+
+
     //Identify fraudulent transactions in a list of transactions (populate the fraudulent transactions),
     /**
      * Mark a transaction fraudulent if:
