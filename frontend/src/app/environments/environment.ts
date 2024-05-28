19e585190139ebc7ad3dbfb2f3e979a9c0f3b863
Varad Patil <varad.patil@hackerrank.com>
2024-05-28 12:15:47 +0530

    fix: Angular App preview on HRW. Cleanup

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 const currentHost = window?.location?.host;
 const currentProtocol = window?.location?.protocol || "http:";
 // const baseURL = `${currentProtocol}//${currentHost.replace("8000", "8080")}`;
-const baseURL = `${currentProtocol}//${currentHost.replace("4200", "8000")}`;
+const baseURL = `${currentProtocol}//${currentHost.replace("3000", "8000")}`;
 
 // const baseURL = 'http://localhost:8000';
 export const environment = {
