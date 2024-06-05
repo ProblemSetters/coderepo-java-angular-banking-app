e0fb2ec75afdd14d16565dbd1ec8f62a8dfdb6be
Anshu-Pandey-Hackerrank <anshu.pandey@problemsetters.com>
2024-06-05 13:18:43 +0530

    add: task6,7,8 solutions

--- backend/src/main/java/com/hackerrank/corebanking/service/CardService.java
+++ backend/src/main/java/com/hackerrank/corebanking/service/CardService.java
@@ -3,7 +3,6 @@ package com.hackerrank.corebanking.service;
 import com.hackerrank.corebanking.model.Card;
 import com.hackerrank.corebanking.repository.CardRepository;
 import com.hackerrank.corebanking.util.CommonUtils;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,80 +10,90 @@ import java.util.List;
 
 @Service
 public class CardService {
-  private final CardRepository cardRepository;
+    private final CardRepository cardRepository;
 
-  @Autowired
-  CardService(CardRepository cardRepository) {
-    this.cardRepository = cardRepository;
-  }
-
-  public Card createNewCard(Card card) {
-    if (card.getCardNumber() != null) {
-      throw new IllegalArgumentException("The cardID must not be provided when creating a new card.");
-    }
-    card.setCardNumber(CommonUtils.generate(16));
-    return cardRepository.save(card);
-  }
-
-  public Card getCardByCardNumber(String cardNumber) {
-    return cardRepository
-            .findCardByCardNumber(cardNumber)
-            .orElseThrow(() -> new IllegalArgumentException("Card with given cardNumber not found."));
-  }
-
-  public List<Card> getAllCards(Long accountId) {
-    return cardRepository
-            .findCardsByAccountId(accountId)
-            .orElseThrow(() -> new IllegalArgumentException("Card with given accountId not found."));
-  }
-
-  public Card deleteCardByCardNumber(String cardNumber) {
-    Card existing = cardRepository
-            .findCardByCardNumber(cardNumber)
-            .orElseThrow(() -> new IllegalArgumentException("Card with given cardNumber not found."));
-    cardRepository.delete(existing);
-
-    return existing;
-  }
-
-  public Card updatePin(String cardNumber, int newPin) {
-    Card existing = cardRepository
-            .findCardByCardNumber(cardNumber)
-            .orElseThrow(() -> new IllegalArgumentException("Card with given cardNumber not found."));
-
-    validatePin(newPin);
-
-    existing.setPin(newPin);
-    cardRepository.save(existing);
-
-    return existing;
-  }
-private void validatePin(int pin) {
-
-    if (pin < 0) {
-        throw new UnsupportedOperationException("Pin must be positive number");
+    @Autowired
+    CardService(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
     }
 
-    String pinString = String.valueOf(pin);
 
-    if (pinString.length() != 4) {
-        throw new UnsupportedOperationException("Pin must be of length 4");
+    public Card createNewCard(Card card) {
+        if (card.getCardNumber() != null) {
+            throw new IllegalArgumentException("The cardID must not be provided when creating a new card.");
+        }
+        card.setCardNumber(CommonUtils.generate(16));
+        return cardRepository.save(card);
     }
 
-    char firstDigit = pinString.charAt(0);
-    boolean isSequence = pinString.chars().allMatch(ch -> ch == firstDigit);
-    if (isSequence) {
-        throw new UnsupportedOperationException("Pin must not be a sequence of the same digits");
+    public Card getCardByCardNumber(String cardNumber) {
+        return cardRepository
+                .findCardByCardNumber(cardNumber)
+                .orElseThrow(() -> new IllegalArgumentException("Card with given cardNumber not found."));
     }
-}
 
-  public Card blockCard(String cardNumber, boolean block) {
-    Card existing = cardRepository
-            .findCardByCardNumber(cardNumber)
-            .orElseThrow(() -> new IllegalArgumentException("Card with given cardNumber not found."));
-    existing.setBlocked(block);
-    cardRepository.save(existing);
+    public List<Card> getAllCards(Long accountId) {
+        return cardRepository
+                .findCardsByAccountId(accountId)
+                .orElseThrow(() -> new IllegalArgumentException("Card with given accountId not found."));
+    }
 
-    return existing;
-  }
+    public Card deleteCardByCardNumber(String cardNumber) {
+        Card existing = cardRepository
+                .findCardByCardNumber(cardNumber)
+                .orElseThrow(() -> new IllegalArgumentException("Card with given cardNumber not found."));
+        cardRepository.delete(existing);
+
+        return existing;
+    }
+
+    public Card updatePin(String cardNumber, int newPin) {
+        Card existing = cardRepository
+                .findCardByCardNumber(cardNumber)
+                .orElseThrow(() -> new IllegalArgumentException("Card with given cardNumber not found."));
+
+        if (0 > newPin) {
+            throw new UnsupportedOperationException("Pin must be positive number");
+        }
+        if (validatePin(newPin)) {
+            throw new UnsupportedOperationException("Pin must not be a sequence of the same digits");
+        }
+        int length = Integer.toString(newPin).length();
+        if (length != 4) {
+            throw new UnsupportedOperationException("Pin must be of length 4");
+        }
+
+        //TODO: pin validation before saving
+        /**
+         * 1. pin must be of length 4.
+         * 2. pin must be integer.
+         * 3. pin must not be a sequence of same digits like 1111, 0000, 3333, etc.
+         * 4. pin must be positive number
+         */
+        existing.setPin(newPin);
+        cardRepository.save(existing);
+
+        return existing;
+    }
+
+    private boolean validatePin(int pin) {
+        String pinString = String.valueOf(pin);
+        for (int i = 1; i < pinString.length(); i++) {
+            if (pinString.charAt(i) != pinString.charAt(0)) {
+                return false; // PIN is not a sequence of the same digits
+            }
+        }
+
+        return true;
+    }
+
+    public Card blockCard(String cardNumber, boolean block) {
+        Card existing = cardRepository
+                .findCardByCardNumber(cardNumber)
+                .orElseThrow(() -> new IllegalArgumentException("Card with given cardNumber not found."));
+        existing.setBlocked(block);
+        cardRepository.save(existing);
+
+        return existing;
+    }
 }
--- backend/src/main/java/com/hackerrank/corebanking/service/TransactionService.java
+++ backend/src/main/java/com/hackerrank/corebanking/service/TransactionService.java
@@ -1,60 +1,90 @@
 package com.hackerrank.corebanking.service;
 
 import com.hackerrank.corebanking.model.Account;
+import com.hackerrank.corebanking.model.Beneficiary;
 import com.hackerrank.corebanking.model.Transaction;
 import com.hackerrank.corebanking.repository.AccountRepository;
 import com.hackerrank.corebanking.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class TransactionService {
-  private final TransactionRepository transactionRepository;
-  private final AccountRepository accountRepository;
+    private final TransactionRepository transactionRepository;
+    private final AccountRepository accountRepository;
+    private final BeneficiaryService beneficiaryService;
 
-  @Autowired
-  TransactionService(TransactionRepository transactionRepository, AccountRepository accountRepository) {
-    this.transactionRepository = transactionRepository;
-    this.accountRepository = accountRepository;
-  }
-
-
-  public Transaction sendMoney(Transaction transaction) {
-    Account fromAccount = accountRepository.findById(transaction.getFromAccountId()).get();
-    Account toAccount = accountRepository.findById(transaction.getToAccountId()).get();
-
-    // Basic Bugfix Balance Validation
-    // Problem Statement:
-    // 1. the sending account must have a balance greater than or equal to the sending amount.
-    // 2. if balance is insufficient, transaction must be rejected and appropriate message to be returned.
-    // 2. the return message should be "Insufficient fund, transaction canceled".
+    @Autowired
+    TransactionService(TransactionRepository transactionRepository, AccountRepository accountRepository, BeneficiaryService beneficiaryService) {
+        this.transactionRepository = transactionRepository;
+        this.accountRepository = accountRepository;
+        this.beneficiaryService = beneficiaryService;
+    }
 
 
-    //Identify fraudulent transactions in a list of transactions (populate the fraudulent transactions),
-    /**
-     * Mark a transaction fraudulent if:
-     *    - the sender is sending same amount 5 times to same receiver within a day.
-     *    - and the receiver is not in beneficiary list and has not sent any mount before.
-     *    - if the receiver is in beneficiary list, then check if it was added just today.
-     */
-    fromAccount.setBalance(fromAccount.getBalance() - transaction.getTransferAmount());
-    toAccount.setBalance(toAccount.getBalance() + transaction.getTransferAmount());
+    public Transaction sendMoney(Transaction transaction) {
+        Account fromAccount = accountRepository.findById(transaction.getFromAccountId()).get();
+        Account toAccount = accountRepository.findById(transaction.getToAccountId()).get();
 
-    accountRepository.save(fromAccount);
-    accountRepository.save(toAccount);
+        Date todayDate = Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant());
+        Date yesterdayDate = Date.from(LocalDate.now().minusDays(1).atStartOfDay(ZoneId.systemDefault()).toInstant());
+        List<Transaction> transactions = transactionRepository.findTransactionsByDateCreatedBetweenAndFromAccountIdOrToAccountId(yesterdayDate, todayDate,
+                fromAccount.getAccountId().toString(), toAccount.getAccountId().toString());
 
-    return transactionRepository.save(transaction);
-  }
+        List<Transaction> todayTransactions = transactions.stream()
+                .filter(t -> t.getToAccountId().equals(toAccount.getAccountId()))
+                .filter(t -> t.getDateCreated().after(Date.from(new Date().toInstant().minus(1, ChronoUnit.DAYS))))
+                .toList();
 
-  public List<Transaction> totalTransactions(Long accountId) {
-    return transactionRepository.findTransactionByFromAccountId(accountId);
-  }
+        transactions.removeAll(todayTransactions);
+
+        List<Beneficiary> beneficiaries = beneficiaryService.getAllBeneficiaries();
+        List<Beneficiary> todayBeneficiaries = beneficiaries.stream()
+                .filter(b -> b.getBeneficiaryAccountId().equals(toAccount.getAccountId()))
+                .filter(b -> b.getDateCreated().after(todayDate))
+                .toList();
+
+        if (todayTransactions.size() > 5 && beneficiaries.isEmpty() && transactions.isEmpty()
+                || !todayBeneficiaries.isEmpty()) {
+            throw new UnsupportedOperationException("Transaction fraudulent");
+        }
+
+        // Basic Bugfix Balance Validation
+        // Problem Statement:
+        // 1. the sending account must have a balance greater than or equal to the sending amount.
+        // 2. if balance is insufficient, transaction must be rejected and appropriate message to be returned.
+        // 2. the return message should be "Insufficient fund, transaction canceled".
 
 
-  public Object getErrorMessage() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getErrorMessage'");
-  }
+        //Identify fraudulent transactions in a list of transactions (populate the fraudulent transactions),
+        /**
+         * Mark a transaction fraudulent if:
+         *    - the sender is sending same amount 5 times to same receiver within a day.
+         *    - and the receiver is not in beneficiary list and has not sent any mount before.
+         *    - if the receiver is in beneficiary list, then check if it was added just today.
+         */
+        fromAccount.setBalance(fromAccount.getBalance() - transaction.getTransferAmount());
+        toAccount.setBalance(toAccount.getBalance() + transaction.getTransferAmount());
+
+        accountRepository.save(fromAccount);
+        accountRepository.save(toAccount);
+
+        return transactionRepository.save(transaction);
+    }
+
+    public List<Transaction> totalTransactions(Long accountId) {
+        return transactionRepository.findTransactionByFromAccountId(accountId);
+    }
+
+
+    public Object getErrorMessage() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getErrorMessage'");
+    }
 }
