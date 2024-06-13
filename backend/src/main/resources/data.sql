210db0f370711aff76d0cc01206befd5ec6cd2e2
Dmytro Karimov <dmytro_karimov@live.ru>
2024-06-13 15:15:01 +0300

    bugfix: Fix transactionId initial counter value

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -144,3 +144,4 @@ insert into transaction(date_created, last_created, transaction_id, from_account
 values (CURRENT_DATE - INTERVAL '4' DAY, CURRENT_DATE - INTERVAL '4' DAY, 1041, 1010113169, 1010213162, 34);
 insert into transaction(date_created, last_created, transaction_id, from_account_id, to_account_id, transfer_amount)
 values (CURRENT_DATE - INTERVAL '4' DAY, CURRENT_DATE - INTERVAL '4' DAY, 1042, 1010113169, 1010213162, 34);
+-- if add more transaction do not forget to change initialValue for Transaction.transactionId, in annotitaion TableGenerator
