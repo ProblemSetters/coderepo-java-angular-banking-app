5fb63788d03e0bba4a19308f91555eb4a16b4934
Shubham Aggarwal <shubham@atomtechnologies.io>
2025-03-20 17:45:30 +0530

    Combined tests into single test file (#34)
    * delete: dark theme toggeler
    
    * Combined tests into single test file
    
    * Few updates to test
    
    ---------
    
    Co-authored-by: Anshu-Pandey-Hackerrank <anshu.pandey@problemsetters.com>
    Co-authored-by: Anshu-Pandey-Hackerrank <141834713+Anshu-Pandey-Hackerrank@users.noreply.github.com>

--- backend/src/test/java/com/hackerrank/corebanking/service/task11/TransactionServiceTest.java
+++ backend/src/test/java/com/hackerrank/corebanking/service/task11/TransactionServiceTest.java
@@ -1,4 +1,3 @@
-
 package com.hackerrank.corebanking.service.task11;
 
 import com.hackerrank.corebanking.exception.FraudTransactionException;
@@ -12,24 +11,32 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class TransactionServiceTest {
 
+    private FraudDetectionService mockFraudDetectionService;
+
     private TransactionService transactionService;
-    private TransactionRepository transactionRepository;
-    private AccountRepository accountRepository;
+    private TransactionRepository mockTransactionRepository;
+    private AccountRepository mockAccountRepository;
     private FraudDetectionService fraudDetectionService;
 
     @BeforeEach
     void setup() {
-        transactionRepository = Mockito.mock(TransactionRepository.class);
-        accountRepository = Mockito.mock(AccountRepository.class);
-        fraudDetectionService = Mockito.mock(FraudDetectionService.class);
-        transactionService = new TransactionService(transactionRepository, accountRepository, null, fraudDetectionService);
+        mockTransactionRepository = mock(TransactionRepository.class);
+        mockAccountRepository = mock(AccountRepository.class);
+        fraudDetectionService = mock(FraudDetectionService.class);
+
+        mockFraudDetectionService = new FraudDetectionService(mockTransactionRepository);
+        transactionService = new TransactionService(mockTransactionRepository, mockAccountRepository, null, fraudDetectionService);
     }
 
     @Test
@@ -37,7 +44,7 @@ public class TransactionServiceTest {
         when(fraudDetectionService.isSuspiciousTransaction(Mockito.any(Transaction.class))).thenReturn(true);
         Account account = new Account();
         account.setLocked(false);
-        when(accountRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(account));
+        when(mockAccountRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(account));
 
         Transaction transaction = new Transaction();
         transaction.setFromAccountId(1L);
@@ -56,9 +63,45 @@ public class TransactionServiceTest {
         Account account = new Account();
         account.setLocked(false);
 
-        when(accountRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(account));
+        when(mockAccountRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(account));
         when(fraudDetectionService.isSuspiciousTransaction(transaction)).thenReturn(true);
 
         assertThrows(FraudTransactionException.class, () -> transactionService.sendMoney(transaction));
+    }
+
+    @Test
+    void testIsFrequentTransaction() {
+        List<Transaction> transactions = Collections.nCopies(6, new Transaction());
+        when(mockTransactionRepository.findTransactionsByFromAccountIdAndDateCreatedAfter(Mockito.anyLong(), Mockito.any(LocalDateTime.class)))
+                .thenReturn(transactions);
+
+        assertTrue(mockFraudDetectionService.isFrequentTransaction(1L));
+    }
+
+    @Test
+    void testIsNotFrequentTransaction() {
+        List<Transaction> transactions = Collections.nCopies(2, new Transaction());
+        when(mockTransactionRepository.findTransactionsByFromAccountIdAndDateCreatedAfter(Mockito.anyLong(), Mockito.any(LocalDateTime.class)))
+                .thenReturn(transactions);
+
+        assertFalse(mockFraudDetectionService.isFrequentTransaction(1L));
+    }
+
+    @Test
+    void testIsSuspiciousMerchant() {
+        Long suspiciousAccountNumber = 9999999991L;
+
+        boolean result = mockFraudDetectionService.isSuspiciousMerchant(suspiciousAccountNumber);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void testIsNotSuspiciousMerchant() {
+        Long nonSuspiciousAccountNumber = 1234567890L;
+
+        boolean result = mockFraudDetectionService.isSuspiciousMerchant(nonSuspiciousAccountNumber);
+
+        assertFalse(result);
     }
 }
